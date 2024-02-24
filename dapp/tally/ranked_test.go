a19d8007c7fc731bda0deabae9ffc8303936b37e
Gabriel Coutinho de Paula <gcdepaula@protonmail.com>
2024-02-24 14:50:33 -0700

    refactor: fix linter warnings

--- dapp/tally/ranked_test.go
+++ dapp/tally/ranked_test.go
@@ -10,11 +10,21 @@ import (
 func TestRankedVote(t *testing.T) {
 	rankedTally := NewRankedTally(3)
 
-	rankedTally.AddVote([]uint{0, 2, 1}, uint256.NewInt(1))
-	rankedTally.AddVote([]uint{1, 0, 2}, uint256.NewInt(1))
-	rankedTally.AddVote([]uint{1, 0, 2}, uint256.NewInt(1))
-	rankedTally.AddVote([]uint{2, 1, 2}, uint256.NewInt(1))
-	rankedTally.AddVote([]uint{2, 0, 1}, uint256.NewInt(1))
+	if err := rankedTally.AddVote([]uint{0, 2, 1}, uint256.NewInt(1)); err != nil {
+		panic(err)
+	}
+	if err := rankedTally.AddVote([]uint{1, 0, 2}, uint256.NewInt(1)); err != nil {
+		panic(err)
+	}
+	if err := rankedTally.AddVote([]uint{1, 0, 2}, uint256.NewInt(1)); err != nil {
+		panic(err)
+	}
+	if err := rankedTally.AddVote([]uint{2, 1, 2}, uint256.NewInt(1)); err != nil {
+		panic(err)
+	}
+	if err := rankedTally.AddVote([]uint{2, 0, 1}, uint256.NewInt(1)); err != nil {
+		panic(err)
+	}
 
 	winner := rankedTally.CloseVoting()
 	fmt.Printf("The winner is: %d\n", winner)
