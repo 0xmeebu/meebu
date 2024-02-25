e263cfd678a187721ece59edb816aa092ff4d6f6
Pedro Argento <pedro.argento@gmail.com>
2024-02-25 16:02:49 -0300

    proposal form as a modal

--- frontend/src/Components/ActionsMenu.tsx
+++ frontend/src/Components/ActionsMenu.tsx
@@ -1,11 +1,13 @@
 import { Group, Button } from '@mantine/core';
 import SideDrawer from './SideDrawer';
+import ProposalFormModal from './ProposalFormModal';
+
 function ActionMenu() {
   return (
     <Group justify="flex-start" gap="sm">
       <SideDrawer/>
       <Button>Register DAO</Button>
-      <Button>Create Proposal</Button>
+      <ProposalFormModal />
 
     </Group>
   );
---
+++ frontend/src/Components/ProposalFormModal.tsx
@@ -0,0 +1,19 @@
+import { useDisclosure } from '@mantine/hooks';
+import { Modal, Button } from '@mantine/core';
+import ProposalCreationForm from './ProposalCreationForm';
+
+function ProposalFormModal() {
+  const [opened, { open, close }] = useDisclosure(false);
+
+  return (
+    <>
+      <Modal opened={opened} onClose={close} title="Authentication">
+        <ProposalCreationForm />
+      </Modal>
+
+      <Button onClick={open}>Create Proposal</Button>
+    </>
+  );
+}
+
+export default ProposalFormModal
