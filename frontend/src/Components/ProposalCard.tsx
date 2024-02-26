f0d02b7669ed6e94860221840264fdd2137e4f11
Pedro Argento <pedro.argento@gmail.com>
2024-02-25 23:45:26 -0300

    refactor interfaces

--- frontend/src/Components/ProposalCard.tsx
+++ frontend/src/Components/ProposalCard.tsx
@@ -1,16 +1,6 @@
 import { Card, Image, Text, Badge, Button, Group, Stack} from '@mantine/core';
 import TokenWeights from './TokenWeights';
-
-interface Erc20Weights {
-  address: string;
-  weight: number;
-  timeWeighted: boolean;
-}
-
-interface Erc721Multipliers {
-  address: string;
-  multiplier: number; // R  epresent the fixed-point number as a regular number, assuming the handling code knows it's in percentage with 2 decimals.
-}
+import { TokenWeight } from "../Interfaces"
 
 
 interface Policy {
@@ -27,24 +17,48 @@ interface ProposalCardProps {
   // ballot: Policy[];
 }
 
+
+const ERC20Weights = [
+  {
+    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+    "weight": 12,
+    "timeWeighted": false
+  },
+  {
+    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+    "weight": 17,
+    "timeWeighted": false
+  },
+  {
+    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    "weight": 0,
+    "timeWeighted": false
+  }
+]
+
 function ProposalCard(props: ProposalCardProps) {
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
 
-        <Stack>
-        <Text size="sm">{props.orgAddress}</Text>
+        <Group>
+          <Text size="sm" fs='italic'>{props.orgAddress}</Text>
           <Group justify="space-between" mt="md" mb="xs">
-            <Text fw={500}>{props.title}</Text>
+            <Text fw={700}>{props.title}</Text>
             <Badge color="pink">{props.tallyingSystem}</Badge>
           </Group>
-        </Stack>
+        </Group>
 
 
       <Text size="sm" c="dimmed">
         {props.description}
       </Text>
 
-      <TokenWeights />
+      <Group gap="sm">
+      <TokenWeights weights={ERC20Weights} cat='Weight'/>
+      <TokenWeights weights={ERC20Weights} cat='Multiplier'/>
+    </Group>
+
+  
 
       <Button color="pink" fullWidth mt="md" radius="md">
         Vote
--- frontend/src/Components/TokenWeights.tsx
+++ frontend/src/Components/TokenWeights.tsx
@@ -1,27 +1,16 @@
 import tokens from "../tokenList";
-import { Avatar, AvatarGroup, List, Tooltip } from '@mantine/core';
+import { Avatar, AvatarGroup, Text, HoverCard } from '@mantine/core';
+import { TokenWeight, TokenList, Token } from "../Interfaces"
+import TokenTable from "./Table";
 
-
-
-interface Erc20Weights {
-  address: string;
-  weight: number;
-  timeWeighted: boolean;
-}
-
-interface Token {
-   label: string; 
-   value: string; 
-   uri: string; 
-}
 
 interface TokenWeightsProps {
-  votes?: [Erc20Weights];
+  weights: TokenWeight[];
+  cat: string
 }
 
 
-function mergeObjects(listA: Erc20Weights[], listB: Token[]): (Erc20Weights & Token)[] {
-  console.log(listA)
+function mergeObjects(listA: TokenWeight[], listB: TokenList[]): (Token)[] {
   const intersection = listA.filter(a => listB.some(b => b.value === a.address));
   return intersection.map(item => {
       const correspondingB = listB.find(b => b.value === item.address)!; // Non-null assertion, modify as needed for safety
@@ -30,35 +19,20 @@ function mergeObjects(listA: Erc20Weights[], listB: Token[]): (Erc20Weights & To
 }
 
 function TokenWeights (props: TokenWeightsProps){
-  let ERC20Weights = [
-    {
-      "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
-      "weight": 12,
-      "timeWeighted": false
-    },
-    {
-      "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
-      "weight": 17,
-      "timeWeighted": false
-    },
-    {
-      "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-      "weight": 0,
-      "timeWeighted": false
-    }
-  ]
-  
-  const votingTokens = mergeObjects(ERC20Weights, tokens);
-  
-  console.log(votingTokens)
+    const votingTokens = mergeObjects(props.weights, tokens);
   return(
-    <Tooltip label="test">
+    <HoverCard width={280} shadow="md">
+        <HoverCard.Target>
       <AvatarGroup>
         {votingTokens.map((token, index) => (
         <Avatar  src={token.uri}/>
             ))}
       </AvatarGroup>
-    </Tooltip>
+      </HoverCard.Target>
+        <HoverCard.Dropdown>
+          <TokenTable weights={votingTokens} catg={props.cat} />
+        </HoverCard.Dropdown>
+      </HoverCard>
   );
 }
 
