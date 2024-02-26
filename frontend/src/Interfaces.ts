a2d3cbe8082248fb213df73f1170addc4e3ed89b
Pedro Argento <pedro.argento@gmail.com>
2024-02-25 23:45:26 -0300

    refactor interfaces

---
+++ frontend/src/Interfaces.ts
@@ -0,0 +1,25 @@
+export interface TokenWeight {
+    address: string;
+    weight: number;
+    timeWeighted: boolean;
+  }
+  
+ export interface TokenList {
+     label: string; 
+     value: string; 
+     uri: string; 
+  }
+
+  export interface Token {
+    label: string; 
+    value: string; 
+    uri: string; 
+    address: string;
+    weight: number;
+    timeWeighted: boolean;
+ }
+
+export interface ballot {
+    description: string;
+    vouchers: string;
+  }
