94c95df8561e18dd80f1c84fe502784b2f54a594
Pedro Argento <pedro.argento@gmail.com>
2024-02-27 14:20:27 -0300

    add dao divider as a separate component

---
+++ frontend/src/Data/state.ts
@@ -0,0 +1,510 @@
+   export const state =  {
+        "orgFactory": "0x1",
+        "orgs": {
+            "0x4B8Cdb9CB66F383578dBE4981c5e47F19C398380": [{
+                "title": "Community Park Renovation Project",
+                "description": "Allocate funds to renovate the local community park, including upgrading playground equipment and adding more benches and green spaces.",
+                "tallyingSystem": 0,
+                 "ERC20Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "ERC721Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "policies":  [
+                  {
+                    "index": 0,
+                    "description": "1ss",
+                    "vouchers": "sdsds"
+                  },
+                  {
+                    "index": 1,
+                    "description": "ffdsdsd",
+                    "vouchers": "sdsdsd"
+                  },
+                  {
+                    "index": 2,
+                    "description": "ssdsdsd",
+                    "vouchers": "sdsdsdsd"
+                  }
+                ]
+            },
+            {
+                "title": "Community Park Renovation Project",
+                "description": "Allocate funds to renovate the local community park, including upgrading playground equipment and adding more benches and green spaces.",
+                "tallyingSystem": 0,
+                 "ERC20Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "ERC721Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "policies":  [
+                  {
+                    "index": 0,
+                    "description": "1ss",
+                    "vouchers": "sdsds"
+                  },
+                  {
+                    "index": 1,
+                    "description": "ffdsdsd",
+                    "vouchers": "sdsdsd"
+                  },
+                  {
+                    "index": 2,
+                    "description": "ssdsdsd",
+                    "vouchers": "sdsdsdsd"
+                  }
+                ]
+            },
+            {
+                "title": "Community Park Renovation Project",
+                "description": "Allocate funds to renovate the local community park, including upgrading playground equipment and adding more benches and green spaces.",
+                "tallyingSystem": 0,
+                 "ERC20Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "ERC721Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "policies":  [
+                  {
+                    "index": 0,
+                    "description": "1ss",
+                    "vouchers": "sdsds"
+                  },
+                  {
+                    "index": 1,
+                    "description": "ffdsdsd",
+                    "vouchers": "sdsdsd"
+                  },
+                  {
+                    "index": 2,
+                    "description": "ssdsdsd",
+                    "vouchers": "sdsdsdsd"
+                  }
+                ]
+            }],
+            "0xr3r3r3rCdb9CB66F3833rBE4981c5e47F19C398380": [{
+                "title": "Community Park Renovation Project",
+                "description": "Allocate funds to renovate the local community park, including upgrading playground equipment and adding more benches and green spaces.",
+                "tallyingSystem": 0,
+                 "ERC20Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "ERC721Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "policies":  [
+                  {
+                    "index": 0,
+                    "description": "1ss",
+                    "vouchers": "sdsds"
+                  },
+                  {
+                    "index": 1,
+                    "description": "ffdsdsd",
+                    "vouchers": "sdsdsd"
+                  },
+                  {
+                    "index": 2,
+                    "description": "ssdsdsd",
+                    "vouchers": "sdsdsdsd"
+                  }
+                ]
+            },
+            {
+                "title": "Community Park Renovation Project",
+                "description": "Allocate funds to renovate the local community park, including upgrading playground equipment and adding more benches and green spaces.",
+                "tallyingSystem": 0,
+                 "ERC20Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "ERC721Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "policies":  [
+                  {
+                    "index": 0,
+                    "description": "1ss",
+                    "vouchers": "sdsds"
+                  },
+                  {
+                    "index": 1,
+                    "description": "ffdsdsd",
+                    "vouchers": "sdsdsd"
+                  },
+                  {
+                    "index": 2,
+                    "description": "ssdsdsd",
+                    "vouchers": "sdsdsdsd"
+                  }
+                ]
+            },
+            {
+                "title": "Community Park Renovation Project",
+                "description": "Allocate funds to renovate the local community park, including upgrading playground equipment and adding more benches and green spaces.",
+                "tallyingSystem": 0,
+                 "ERC20Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "ERC721Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "policies":  [
+                  {
+                    "index": 0,
+                    "description": "1ss",
+                    "vouchers": "sdsds"
+                  },
+                  {
+                    "index": 1,
+                    "description": "ffdsdsd",
+                    "vouchers": "sdsdsd"
+                  },
+                  {
+                    "index": 2,
+                    "description": "ssdsdsd",
+                    "vouchers": "sdsdsdsd"
+                  }
+                ]
+            }],
+            "0x4B8Cdb9CB66F383578dBdsdsdsd981c5e47F19C398380": [{
+                "title": "Community Park Renovation Project",
+                "description": "Allocate funds to renovate the local community park, including upgrading playground equipment and adding more benches and green spaces.",
+                "tallyingSystem": 0,
+                 "ERC20Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "ERC721Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "policies":  [
+                  {
+                    "index": 0,
+                    "description": "1ss",
+                    "vouchers": "sdsds"
+                  },
+                  {
+                    "index": 1,
+                    "description": "ffdsdsd",
+                    "vouchers": "sdsdsd"
+                  },
+                  {
+                    "index": 2,
+                    "description": "ssdsdsd",
+                    "vouchers": "sdsdsdsd"
+                  }
+                ]
+            },
+            {
+                "title": "Community Park Renovation Project",
+                "description": "Allocate funds to renovate the local community park, including upgrading playground equipment and adding more benches and green spaces.",
+                "tallyingSystem": 0,
+                 "ERC20Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "ERC721Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "policies":  [
+                  {
+                    "index": 0,
+                    "description": "1ss",
+                    "vouchers": "sdsds"
+                  },
+                  {
+                    "index": 1,
+                    "description": "ffdsdsd",
+                    "vouchers": "sdsdsd"
+                  },
+                  {
+                    "index": 2,
+                    "description": "ssdsdsd",
+                    "vouchers": "sdsdsdsd"
+                  }
+                ]
+            },
+            {
+                "title": "Community Park Renovation Project",
+                "description": "Allocate funds to renovate the local community park, including upgrading playground equipment and adding more benches and green spaces.",
+                "tallyingSystem": 0,
+                 "ERC20Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "ERC721Weights": [
+                  {
+                    "address": "0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd",
+                    "weight": 12,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0x08d967bb0134F2d07f7cfb6E246680c53927DD30",
+                    "weight": 17,
+                    "timeWeighted": false
+                  },
+                  {
+                    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                    "weight": 0,
+                    "timeWeighted": false
+                  }
+                ],
+                "policies":  [
+                  {
+                    "index": 0,
+                    "description": "1ss",
+                    "vouchers": "sdsds"
+                  },
+                  {
+                    "index": 1,
+                    "description": "ffdsdsd",
+                    "vouchers": "sdsdsd"
+                  },
+                  {
+                    "index": 2,
+                    "description": "ssdsdsd",
+                    "vouchers": "sdsdsdsd"
+                  }
+                ]
+            }]
+        }
+
+    }
